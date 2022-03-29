1e688ae8aac15c5e6b4e4b1b5376d2e13f2d88a6
Carlos Mañas <carlos@spacenomads.com>
2022-03-29 19:07:38 +0200

    Add ej00 I & II

--- ej00-simpsons/src/components/MemberCard.jsx
+++ ej00-simpsons/src/components/MemberCard.jsx
@@ -1,14 +1,31 @@
-function MemberCard(props) {
-  const {name, description, img} = props;
-  return (
-    <div className="simpson-card">
-      <div className="simpson-card__details">
-        <h2 className="simpson-card__name">{name}</h2>
-        <div className="simpson-card__description">{description}</div>
+import React from 'react';
+class MemberCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const currentState = this.state.visible;
+    this.setState({visible: !currentState})
+  }
+
+  render() {
+
+    const {name, description, img} = this.props;
+    return (
+      <div className={`simpson-card ${this.state.visible ? 'simpson-card--visible' : ''}`} onClick={this.handleClick}>
+        <div className="simpson-card__details">
+          <h2 className="simpson-card__name">{name}</h2>
+          <div className="simpson-card__description">{description}</div>
+        </div>
+        <img className="simpson-card__image" src={`./img/${img}`} alt={name} />
       </div>
-      <img className="simpson-card__image" src={`./img/${img}`} alt={name} />
-    </div>
-  );
+    );
+  }
 }
 
 export {MemberCard};
