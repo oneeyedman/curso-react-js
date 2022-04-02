cf8247fd073009ee7b7eb954fd71204d663069a1
Carlos Mañas <carlos@spacenomads.com>
2022-04-02 11:25:16 +0200

    Add "done" basic styles

--- ej01-todo/src/components/Task.js
+++ ej01-todo/src/components/Task.js
@@ -1,18 +1,20 @@
 import { connect } from "react-redux";
-import { completeAction } from "../state/actions";
+import { completeAction, toggleCompletionAction } from "../state/actions";
 import { formatDate } from "../utils/date";
 
 function Task(props) {
   const { label, id, done, completed_date } = props;
   const handleStatusChange = event => {
     const taskID = event.currentTarget.id;
-    props.complete(taskID);
+    props.toggle(taskID);
   }
   return (
     <div className={`task ${done ? 'task--done':''}`}>
-      <input id={ id } name="tasks" type="checkbox" onChange={handleStatusChange} checked={done} />
-      <label htmlFor={ id }>{ label }</label>
-      {done && <time dateTime={ completed_date }>{ formatDate(completed_date) }</time>}
+      <input id={ id } name="tasks" className="task__field" type="checkbox" onChange={handleStatusChange} checked={done} />
+      <label htmlFor={ id } className="task__label">{ label }</label>
+      {done && <time dateTime={ completed_date } className="task__completion-date">{ formatDate(completed_date) }</time>}
+
+      <button type="button" className="task__delete-me">Borrar tarea</button>
     </div>
   );
 }
@@ -24,7 +26,10 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return { complete: (id) => dispatch(completeAction(id)) };
+  return {
+    complete: (id) => dispatch(completeAction(id)),
+    toggle: (id) => dispatch(toggleCompletionAction(id)),
+   };
 };
 
 export default connect(mapStateToProps,mapDispatchToProps)(Task);
