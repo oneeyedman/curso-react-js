be6e11a10052e64acfae87a0757f987485d2c100
Carlos Mañas <carlos@spacenomads.com>
2022-04-05 16:50:44 +0200

    Use state hook with a functional component

--- ej04-todo/src/components/NewTodo.js
+++ ej04-todo/src/components/NewTodo.js
@@ -1,56 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { addAction } from "../state/actions";
 import { connect } from "react-redux";
 import { getNextID } from '../utils/lists';
 
-class NewTodo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      task_name: ''
-    };
-  }
+function NewTodo(props) {
+  const [task_name, setTask_name] = useState('');
 
-  getTaskName = (event) => {
-    const name = event.currentTarget.value;
-    this.setState({task_name: name});
-  }
-
-  handleNewTodoAddition = event => {
-    const taskName = this.state.task_name.trim();
-    const newID = getNextID(this.props.list);
+  const handleNewTodoAddition = event => {
+    const taskName = task_name.trim();
+    const newID = getNextID(props.list);
     if (taskName) {
       console.log(`La nueva tarea se llama ${taskName}`)
-     this.props.add({
+     props.add({
        id: newID,
        creation_date: new Date().toISOString(),
        label: taskName,
        done: false,
        completion_date: null
      });
-     this.setState({task_name: ''})
+     setTask_name('')
     } else {
       console.log('Que qué?');
     }
   }
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.handleNewTodoAddition()
+    handleNewTodoAddition()
   }
 
-  render() {
-    return (<form action="" className="app__add-todo-form form" onSubmit={ this.handleSubmit }>
+
+  const getTaskName = event => {
+    const name = event.currentTarget.value;
+    setTask_name(name);
+  }
+  return (
+    <form action="" className="app__add-todo-form form" onSubmit={ handleSubmit }>
       <div className="form__row">
         <label htmlFor="newTodoItem" className="form__label sr">Añadir nueva tarea</label>
-        <input id="newTodoItem" name="newTodoItem" className="form__field" type="text" onChange={this.getTaskName}
-               value={this.state.task_name}/>
+        <input id="newTodoItem" name="newTodoItem" className="form__field" type="text" onChange={getTaskName}
+               value={task_name}/>
       </div>
       <div className="form__row form__row--action">
-        <button className="form__button form__add" type="button" onClick={this.handleNewTodoAddition}>Añadir</button>
+        <button className="form__button form__add" type="button" onClick={handleNewTodoAddition}>Añadir</button>
       </div>
-    </form>);
-  }
+    </form>
+  );
 }
 
 const mapStateToProps = (state) => {
